04d130cfeeda4cc738529bc90dc12458bac2069c
Lyova Parsyan <parsyan.lyova@gmail.com>
2024-07-07 17:06:05 +0400

    fix gitignore

--- src/__main__.py
+++ src/__main__.py
@@ -6,7 +6,7 @@ from src.image_generator import ImageGridCreator
 def main() -> None:
 
     image_grid_creator = ImageGridCreator(
-        input_dir=configs.DIR_CONFIG.FILES_DIR,
+        input_dir=configs.DIR_CONFIG.INPUT_DIR,
         output_file=configs.DIR_CONFIG.RESULT_TIF
     )
     image_grid_creator.check_dirs()
--- src/config.py
+++ src/config.py
@@ -11,7 +11,7 @@ ENV_DIR = join(project_dir, '.env')
 class DirConfigs(BaseSettings):
     ROOT_DIR: str = os.path.dirname(os.path.dirname(BASE_DIR))
     SRC_DIR: str = os.path.dirname(BASE_DIR)
-    FILES_DIR: str = join(SRC_DIR, 'Для тестового')
+    INPUT_DIR: str = join(SRC_DIR, 'Для тестового')
     RESULT_FILE_DIR: str = join(SRC_DIR, 'result')
     RESULT_TIF: str = join(RESULT_FILE_DIR, 'result.tiff')
 
